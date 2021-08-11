a842108f06a0aef515868fcf7eabb92e9e46136d
Sarthak Ladhwe <sarthak.ladhwe156@gmail.com>
2021-08-11 18:26:04 +0530

    Some styling

---
+++ items.js
@@ -0,0 +1,18 @@
+const items = {
+    food1: {
+        food: 'Classic Burgir',
+        price: '₹69.69',
+    },
+    food2: {
+        food: 'Pitza',
+        price: '₹69.69',
+    },
+    food3: {
+        food: 'Kola',
+        price: '₹69.69',
+    },
+    food4: {
+        food: 'Chicken Leg Piece',
+        price: '₹69.69',
+    }
+};
