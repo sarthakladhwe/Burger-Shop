77f042bb1c62cefaa4a9f5dac4d06a7f4253b31d
Sarthak Ladhwe <sarthak.ladhwe156@gmail.com>
2021-08-15 13:21:13 +0530

    Add to cart

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+function openCart() {
+    document.querySelector(".cartSidebar").style.width = "250px";
+    document.querySelector(".container").style.marginRight = "250px";
+  }
 
+function closeNav() {
+    document.querySelector(".cartSidebar").style.width = "0";
+    document.querySelector(".container").style.marginRight = "0";
+}
